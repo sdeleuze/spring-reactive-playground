40e19edc46d393c8a9036728be050474591a5930
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-07-21 13:47:13 +0200

    Only SSE by default

--- src/main/java/playground/Application.java
+++ src/main/java/playground/Application.java
@@ -17,12 +17,16 @@
 package playground;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 /**
  * @author Sebastien Deleuze
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) throws Exception {
